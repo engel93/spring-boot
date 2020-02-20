3e71017a68cd1b311bf666d5495ef9edfd395fe9
Davi-o <alvesdelima.davi@gmail.com>
2020-02-19 21:28:59 -0300

    first commit

---
+++ src/main/java/com/acelera/java/acelerajavaspringboot/AceleraJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.acelera.java.acelerajavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceleraJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AceleraJavaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/acelera/java/acelerajavaspringboot/Controller/ClientesController.java
@@ -0,0 +1,25 @@
+package com.acelera.java.acelerajavaspringboot.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController()
+@RequestMapping("/clientes")
+public class ClientesController {
+
+//    @RequestMapping(name = "/clientes", method = RequestMethod.GET)
+    @GetMapping(name = "/clientes")
+    public ResponseEntity<List<Clientes>> getClientes(){
+        List<Clientes> clientes = new ArrayList<Clientes>();
+        clientes.add( new Clientes("Nome1", "CPF1"));
+        clientes.add( new Clientes("Nome2", "CPF2"));
+        return ResponseEntity.ok(clientes);
+    }
+
+}
